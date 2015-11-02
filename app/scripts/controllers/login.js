d873a51ada143f52a69578b12c2b0d79b82eb7b6
jtrichak <jtrichak@purdue.edu>
2015-11-02 12:03:27 -0600

    Joe's Changes
    Main Page Done w/ formatting and design, redirects to event and culture
    HTML pages
    Login Button made in Login page

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name krinaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the krinaApp
+ */
+angular.module('krinaApp')
+  .controller('LoginCtrl', function ($scope) {
+  
+  });
