7816cc119126df310f8118643cac00e62e163f20
jtrichak <jtrichak@purdue.edu>
2015-11-09 11:58:01 -0600

    Made the Authentication
    Username and Password for Firebase will be on sharepoint

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -9,5 +9,18 @@
  */
 angular.module('krinaApp')
   .controller('LoginCtrl', function ($scope) {
-  
-  });
+  $scope.login = function() {
+  var myDataRef = new Firebase('https://dazzling-inferno-9963.firebaseio.com');
+    myDataRef.authWithPassword({
+    	"email": $scope.email,
+    	"password": $scope.password
+    }, function(error, authData) {
+    	if (error) {
+    		console.log("Login Failed!", error);
+    	} else{
+    		console.log("Authenticated successfully!", authData);
+    		//Redirect to main page here
+    	}
+    });
+  };
+});
