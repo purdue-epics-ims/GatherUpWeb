44213cef72cace8a8084d8701359f269a2371963
Honkwan Wu <wu474@purdue.edu>
2016-02-27 13:18:21 -0500

    sort by date

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -11,15 +11,21 @@ angular.module('krinaApp')
   .controller('EventsCtrl', function ($scope) {
   $scope.eventsubmit = function() {
   	var myEventRef = new Firebase('https://dazzling-inferno-9963.firebaseio.com/event');
+    
     var createGuid = function () {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
       return v.toString(16);
     })};
+    var dateID = $scope.eventdate.getFullYear()*366+$scope.eventdate.getMonth()*31+$scope.eventdate.getDate();
+    /*if($scope.eventdate.getYear()!=0){
+      dateID = $scope.eventdate.getYear()*366+$scope.eventdate.getMonth()*31+$scope.eventdate.getDay();
+    }*/
     var eventObject = {
       "name": $scope.eventname,
       "description": $scope.eventdescription,
       "date": $scope.eventdate.toDateString(),
+      "dateID": dateID,
       "schema": "event"
     }
     myEventRef.push(eventObject);
