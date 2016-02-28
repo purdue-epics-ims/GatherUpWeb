35fa521eac3450789b161997f81b45035221a40d
Heng Li <li1784@purdue.edu>
2016-02-27 22:15:49 -0500

    Added alert and refresh for submit button

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -11,7 +11,7 @@ angular.module('krinaApp')
   .controller('EventsCtrl', function ($scope) {
   $scope.eventsubmit = function() {
   	var myEventRef = new Firebase('https://dazzling-inferno-9963.firebaseio.com/event');
-    
+
     var createGuid = function () {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
@@ -29,5 +29,7 @@ angular.module('krinaApp')
       "schema": "event"
     }
     myEventRef.push(eventObject);
+    alert("New event created!");
+    location.reload();
 }
 });
