3ffbef5ec40e2af399b95d233f98bcff5fb3202e
Heng Li <li1784@purdue.edu>
2016-03-06 22:47:36 -0500

    Login page updated. Navigation updating.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -9,17 +9,20 @@
  */
 angular.module('krinaApp')
   .controller('LoginCtrl', function ($scope) {
-  $scope.login = function() {
-  var myDataRef = new Firebase('https://dazzling-inferno-9963.firebaseio.com');
+  $scope.login = function(ref) {
+   var myDataRef = new Firebase('https://dazzling-inferno-9963.firebaseio.com');
     myDataRef.authWithPassword({
-    	"email": $scope.email,
-    	"password": $scope.password
+    	'email': $scope.email,
+    	'password': $scope.password
     }, function(error, authData) {
     	if (error) {
-    		window.alert("Login failed, please enter a valid username and password!", error);
-    	} else{
-    		console.log("Authenticated successfully!", authData);
-        window.location="http://localhost:9000/#/"
+    		//window.alert("Login failed, please enter a valid username and password!", error);
+        window.alert('Login failed, please enter a valid username and password!');
+      } else{
+    		console.log('Authenticated successfully!', authData);
+        //alert("Authenticated successfully!");
+        var temp = document.getElementById(ref).value;
+        window.location = temp;
     	}
     });
   };
