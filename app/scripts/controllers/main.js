4bd8bbcbaf572ac13fa745244eeccfb042bb7225
Heng Li <li1784@purdue.edu>
2016-03-17 23:16:46 -0400

    Updating

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,11 @@
  */
 angular.module('krinaApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.jump=function(){
+      window.location = "#/login"
+      var temp = document.getElementById("ref");
+      temp.value = "#/events";
+  };
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
